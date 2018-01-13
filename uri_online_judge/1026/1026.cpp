f99a0e3513f1745ece0562aa94202af38eee1755
Emanuel Huber <emanuel.tesv@gmail.com>
2018-01-13 20:36:59 -0200

    Create URI To Carry or not to Carry 1026.cpp

---
+++ uri_online_judge/1026/1026.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	unsigned int a, b;
+	
+	while (cin >> a >> b)
+	{
+		unsigned int c = a ^ b;
+		cout << c << endl;
+	}
+		
+	return 0;
+}
