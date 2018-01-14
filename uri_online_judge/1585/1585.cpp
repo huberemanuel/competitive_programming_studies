5e07e4efe9c0496136ead87028ac02cfe252f74e
Emanuel Huber <emanuel.tesv@gmail.com>
2018-01-14 13:37:54 -0200

    Create URI Making Kites 1585.cpp

---
+++ uri_online_judge/1585/1585.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	cin >> n;
+	
+	while (n--)
+	{
+		int x, y;
+		
+		cin >> x >> y;
+		
+		cout << (int)((y / 2.0) * x) << " cm2" << endl;
+		
+	}
+	
+	return 0;
+}
