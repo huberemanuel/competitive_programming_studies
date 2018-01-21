705b1c0ecbd775ef9dc81be40e0b7350dcdd2748
Emanuel Huber <emanuel.tesv@gmail.com>
2018-01-21 21:34:42 -0200

    Fix 12356.cpp

--- uva_online_judge/12356.cpp
+++ uva_online_judge/12356.cpp
@@ -2,45 +2,39 @@
 
 using namespace std;
 
+int S, B, L, R;
+int LS[1000100];
+int RS[1000100];
+
 int main()
 {
 	int s, b;
 	
 	while (cin >> s >> b and s and b)
 	{
-		vector<bool> soldiers (s, true);
 		
+                for (int i = 0; i < s + 1; i++)
+                {   
+                     LS[i] = i - 1;
+                     RS[i] = i + 1;
+                }
+
 		while (b--)
 		{
 			int begin, end;
 			
 			cin >> begin >> end;
+	
+			if (LS[begin] < 1) cout << "* ";
+			else cout << LS[begin] << " ";
 			
-			for (int i = begin - 1; i < end; i++)
-				soldiers[i] = false;
+			if (RS[end] > s) cout << "*";
+			else cout << RS[end];
 			
-			int left = -1, right = -1;
-			
-			for (int i = begin - 1; i >= 0; i--)
-				if (soldiers[i])
-				{
-					left = i + 1;
-					break;
-				}
-					
-			for (int i = end; i < static_cast<int>(soldiers.size()); i++)
-				if (soldiers[i])
-				{
-					right = i + 1;
-					break;
-				}
-				
-			if (left == -1) cout << "* ";
-			else cout << left << " ";
-			
-			if (right == -1) cout << "*";
-			else cout << right;
-			
+
+                        LS[RS[end]] = LS[begin];
+                        RS[LS[begin]] = RS[end];
+
 			cout << endl;
 		}
 		
