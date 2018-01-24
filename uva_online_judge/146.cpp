1d0354ab470e59cef3f545d64aa701baa1f75ff5
Emanuel Huber <emanuel.tesv@gmail.com>
2018-01-24 02:22:34 -0200

    Create UVA 146 ID Codes

---
+++ uva_online_judge/146.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	string code;
+	
+	while (cin >> code and code != "#")
+	{
+		if (not	next_permutation(code.begin(), code.end()))
+			cout << "No Successor." << endl;
+		else 
+			cout << code << endl;
+	}
+	
+	
+	return 0;
+}
