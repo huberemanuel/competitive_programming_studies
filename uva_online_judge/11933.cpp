0c6955d1388c13ff32b02900238c53c215497066
Emanuel Huber <emanuel.tesv@gmail.com>
2018-02-10 22:53:52 -0200

    Refactoring UVA 11933

--- uva_online_judge/11933.cpp
+++ uva_online_judge/11933.cpp
@@ -1,38 +1,38 @@
-#include <bits/stdc++.h>
+	#include <bits/stdc++.h>
 
-using namespace std;
+	using namespace std;
 
-int foo(vector<int> indexes, int n, int i)
-{
-	int num = 0;
-	for (; i < static_cast<int>(indexes.size()); i += 2)
+	int foo(vector<int> indexes, int n, int i)
 	{
-		num = num | (1 << indexes[i]);
-	}
-	return num;
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	int n;
-	
-	while (cin >> n, n)
-	{
-		vector<int> indexes;
-		
-		for (int i = 0; i < 32; i++)
+		int num = 0;
+		for (; i < static_cast<int>(indexes.size()); i += 2)
 		{
-			if (((1 << i) & n) == (1 << i))
-				indexes.push_back(i);
+			num = num | (1 << indexes[i]);
+		}
+		return num;
+	}
+
+	int main()
+	{
+		ios_base::sync_with_stdio(false);
+		cin.tie(NULL);
+		
+		int n;
+		
+		while (cin >> n, n)
+		{
+			vector<int> indexes;
+			
+			for (int i = 0; i < 32; i++)
+			{
+				if (((1 << i) & n) == (1 << i))
+					indexes.push_back(i);
+			}
+			
+			cout << foo(indexes, n, 0) << " " << foo(indexes, n, 1) << endl;
+			
 		}
 		
-		cout << foo(indexes, n, 0) << " " << foo(indexes, n, 1) << endl;
 		
+		return 0;
 	}
-	
-	
-	return 0;
-}
