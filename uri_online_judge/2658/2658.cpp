6536cf7a8090dd03c77c54410278be6694a37d82
Emanuel Huber <emanuel.tesv@gmail.com>
2018-08-19 14:59:14 -0300

    Add Uri 2658

--- uri_online_judge/2658/2658.cpp
+++ uri_online_judge/2658/2658.cpp
@@ -2,52 +2,114 @@
 
 using namespace std;
 
-const int N = 1e5;
-int n;
-int h = sizeof(int) * 8 - __builtin_clz(n);
+#define MAX (int) 2e5
+void comp(int *a, int *b) {
+	for (int i = 0;i < 9; ++i) a[i] += b[i];
+}
 
-struct Node {
-	int freq[9];
-	Node() {
-		memset(freq, 0, sizeof(freq));
+int most_frequent(int *arr) {
+	int index = 0, value = arr[0];
+	for (int i = 1;i < 9; ++i) {
+		if (arr[i] >= value) {
+			index = i;
+			value = arr[i];
+		}
 	}
-	int getFreq() {
-		return max_element(freq, freq + 9) - freq;
+	return index;
+}
+
+class SegmentTree {
+private:
+	int st[MAX<<1][9];
+	int lazy[MAX<<1];
+	int size;
+	#define left(p) (p << 1)
+	#define right(p) ((p << 1) + 1)
+	void build(int p, int l, int r) {
+		if (l == r) { memset(st[p], 0, sizeof(st[p])); st[p][1] = 1; return; }
+		int m = (l + r) / 2;
+		build(left(p), l, m);
+		build(right(p), m+1, r);
+		memset(st[p], 0, sizeof(st[p]));
+		comp(st[p], st[left(p)]);
+		comp(st[p], st[right(p)]);
+	}
+	void push(int p, int l, int r) {
+		
+		if (lazy[p] == 0) return;
+
+		int aux[9];
+		for (int i = 0;i < 9; ++i) aux[i] = st[p][i];
+		for (int i = 0;i < 9; ++i) {
+			st[p][(lazy[p]+i)%9] = aux[i];
+		}
+		
+		if (l != r) {
+			lazy[right(p)] = (lazy[right(p)] + lazy[p]) % 9;
+			lazy[left(p)] = (lazy[left(p)] + lazy[p]) % 9;
+		}
+		
+		lazy[p] = 0;
+	}
+	void update(int p, int l, int r, int a, int b, int k) {
+		push(p, l, r);
+		if (a > r || b < l) return;
+		else if (l >= a && r <= b) {
+			lazy[p] = k; push(p, l, r); return;
+		}
+		update(left(p), l, (l + r) / 2, a, b, k);
+		update(right(p), (l + r) / 2 + 1, r, a, b, k);
+		memset(st[p], 0, sizeof(st[p]));
+		comp(st[p], st[left(p)]);
+		comp(st[p], st[right(p)]);
+	}
+	void query(int p, int l, int r, int a, int b, int *ans) {
+		push(p, l, r);
+		if (a > r || b < l) return;
+		if (l >= a && r <= b) comp(ans, st[p]);
+		else {
+			int m = (l + r) / 2;
+			query(left(p), l, m, a, b, ans);
+			query(right(p), m+1, r, a, b, ans);
+		}
+	}
+public:
+	SegmentTree(int n) {
+		size = n;
+		memset(lazy, 0, sizeof(lazy));
+		build(1, 0, size - 1);
+	}
+	int query(int a, int b, int *ans) { 
+		query(1, 0, size - 1, a, b, ans); 
+		return most_frequent(ans);
+	}
+	void update(int a, int b) { 
+		int ans[9];
+		memset(ans, 0, sizeof(ans));
+		query(a, b, ans);
+		int note = most_frequent(ans);
+		update(1, 0, size - 1, a, b, note); 
 	}
 };
 
-Node t[N<<1];
-int lazy[N<<1];
+int main() {
 
-Node merge(Node a, Node b) {
-	Node c;
-	for (int i = 0; i < 9; ++i) c.freq[i] = a.freq[i] + b.freq[i];
-	return c;
-}
+	int n, q; cin >> n >> q;
+	
+	SegmentTree st(n);
 
-void build() {
-	for (int i = 2*n; i >= n; --i) t[i].freq[1] = 1;
-	for (int i = n - 1; i; --i) t[i] = merge(t[i<<1], t[i<<1|1]);
-
-}
-
-int main()
-{
-	int q; cin >> n >> q;
-	h = sizeof(int) * 8 - __builtin_clz(n);
-	build();
-
-	for (int i = 0;i < 9; ++i) cout << t[1].freq[i] << "\t";
-	cout << endl << endl;
-	for (int i = 0;i < 2 * n; ++i) cout << t[i].getFreq() << "\t";
-
-	for (int i = 0; i < q; ++i) {
-		int a, b; cin >> a >> b;
-		//update();
+	while (q--) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		st.update(a, b);
 	}
-
-	//for (int i = n; i < 2 * n; ++i)
-	//	cout << max_element(t[i], t[i] + 10) << endl;
+	
+	int ans[9];
+	for (int i = 0;i < n; ++i) {
+		memset(ans, 0, sizeof(ans));
+		cout << st.query(i, i, ans) << endl;
+	}
 
 	return 0;
 }
+
