2825fbeda2430a9bccd4544c4a9ee4ba63389b4d
ehs <emanuel.tesv@gmail.com>
2018-12-28 00:23:10 -0200

    Add Codeforces 281 D2 A.cpp

---
+++ codeforces/281/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	string s;
+	getline(cin, s);
+
+	if (s[0] >= 'a' and s[0] <= 'z') s[0] -= 32;
+
+	cout << s << endl;
+
+	return 0;
+}
