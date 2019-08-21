0866c9793cdf48ab976889b51af339d728972afd
ehs <emanuel.tesv@gmail.com>
2019-08-21 11:07:49 -0300

    Add knapsack without memoization cpp

--- classical_algorithms/knapsack/knapsack.cpp
+++ classical_algorithms/knapsack/knapsack.cpp
@@ -20,10 +20,24 @@ int knapsack(int weight, int index)
     return dp[index][weight];
 }
 
+int knapsack2(int weight, int index)
+{
+    if (index < 0) return 0;
+
+    else if (weight - w[index] < 0)
+        return  knapsack(weight, index - 1);
+
+    else
+        return  std::max(knapsack(weight, index - 1),
+                                v[index] + knapsack(weight - w[index], index - 1)
+                            );
+}
+
+
 int main()
 {
     std::memset(dp, -1, sizeof(dp));
-    std::cout << "Max value: " << knapsack(maxWeight, 2) << std::endl;
+    std::cout << "Max value: " << knapsack2(maxWeight, 2) << std::endl;
 
     return 0;
 }
